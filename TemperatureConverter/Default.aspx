8ed16877a7376481228675a1c2f6f293630288ae
JimmyFrancis <james_francis@shaw.ca>
2019-08-21 13:56:00 -0600

    Add project files.

---
+++ TemperatureConverter/Default.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TemperatureConverter.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>The Ultimate Technology</title>
+   <link rel="stylesheet" href = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+    <link href ="temperature.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container-fluid">
+  <div class="jumbotron">
+          <h1>MAGNUM CONVERTER 5000 </h1> </div>
+  
+  <div class="row">
+    <div class="col-sm-4"><img alt="thermometer"  src="images/gauge.jpg" /><br /></div>
+    <div class="col-sm-8"><asp:Label ID="Label1" runat="server" Text="FROM"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label2" runat="server" Text="TO"></asp:Label>
+        <br />
+        <asp:DropDownList ID="ddlFrom" runat="server">
+            <asp:ListItem>Celsius</asp:ListItem>
+            <asp:ListItem>Fahrenheit</asp:ListItem>
+            <asp:ListItem>Kelvin</asp:ListItem>
+        </asp:DropDownList>
+        <asp:DropDownList ID="ddlTo" runat="server">
+            <asp:ListItem>Fahrenheit</asp:ListItem>
+            <asp:ListItem>Celsius</asp:ListItem>
+            <asp:ListItem>Kelvin</asp:ListItem>
+        </asp:DropDownList>
+        <br />
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="Input Temperature"></asp:Label>
+&nbsp;<asp:TextBox ID="txtTemp" runat="server"></asp:TextBox>
+        <asp:RangeValidator ID="RangeValidator1" runat="server" Display="Dynamic" ErrorMessage="Must be between -1000 and 1000" ForeColor="#CC0000" MaximumValue="1000" MinimumValue="-1000" Type="Integer" ControlToValidate="txtTemp"></asp:RangeValidator>
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" Display="Dynamic" ErrorMessage="Please add a number!" ForeColor="#CC0000" ControlToValidate="txtTemp"></asp:RequiredFieldValidator>
+        <br />
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="Output Temperature"></asp:Label>
+&nbsp;<asp:Label ID="lblResult" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:Button class="btn btn-primary btn-lg" ID="btnCalculate" runat="server" Height="44px" OnClick="btnCalculate_Click" Text="Calculate" Width="114px" />
+        &nbsp;&nbsp;&nbsp;
+        <asp:Button class="btn btn-primary btn-lg" ID="btnClear" runat="server" Height="44px" OnClick="btnClear_Click" Text="Clear" Width="114px" CausesValidation="False" /></div>
+  </div>
+</div>
+               
+    </form>
+</body>
+</html>
---
+++ TemperatureConverter/Default.aspx.cs
@@ -0,0 +1,110 @@
+﻿/**
+ * Lab 1 ASP.NET
+ * Temperature Converter Web Appliction
+ * By James Francis
+ * July 2019
+ * */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TemperatureConverter
+{
+    // Celsius to Kelvin : [K] = [°C] + 273.15
+    // Kelvin to Celsius : [°C] = [K] − 273.15
+    // Fahrenheit to Kelvin : [K] = ([°F] + 459.67) × 5⁄9
+    // Kelvin to Fahrenheit : [°F] = [K] × 9⁄5 − 459.67
+    public partial class Default : System.Web.UI.Page
+    {
+        const double KELVIN = 273.15;
+        const double F_KELVIN = 459.67;
+        const double FAHR = 32;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //turn off unobtrusive validation
+            UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e) //clear controls
+        {
+            ddlFrom.SelectedIndex = 0;
+            ddlTo.SelectedIndex = 0;
+            txtTemp.Text = "";
+            lblResult.Text = "";
+          //  txtTemp.Focus();
+        }
+
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double inputTemp = Convert.ToDouble(txtTemp.Text);
+            double resultTemp = 0;
+
+            switch (ddlTo.SelectedItem.ToString())
+            {
+                case "Celsius": //convert to Celsius
+                    switch (ddlFrom.SelectedItem.ToString())
+                    {
+                        case "Fahrenheit":
+                            resultTemp = (inputTemp - FAHR) * 5 / 9;
+                            break;
+
+                        case "Celsius":
+                            resultTemp = inputTemp; 
+                            break;
+
+                        case "Kelvin":
+                            resultTemp = inputTemp - KELVIN;
+                            break;
+                    }
+                    lblResult.Text = resultTemp.ToString("n2") + " " + (char)176 + "C";
+                    break;
+                    
+                case "Fahrenheit": //convert to Fahrenheit
+                    switch (ddlFrom.SelectedItem.ToString())
+                    {
+                        case "Fahrenheit":
+                            resultTemp = inputTemp;
+                            break;
+
+                        case "Celsius":
+                            resultTemp = inputTemp * 1.8 + 32;
+                            break;
+
+                        case "Kelvin":
+                            resultTemp = (inputTemp * 9 / 5) - F_KELVIN;
+                            break;
+                    }
+                    lblResult.Text = resultTemp.ToString("n2") + " " + (char)176 + "F";
+                    break;
+
+                case "Kelvin": //convert to Kelvin
+                    switch (ddlFrom.SelectedItem.ToString())
+                    {
+                        case "Fahrenheit":
+                            resultTemp = ((inputTemp - FAHR) * 5 / 9) + KELVIN;
+                            resultTemp = (inputTemp + F_KELVIN) * 5 / 9;
+                            break;
+
+                        case "Celsius":
+                            resultTemp = inputTemp + KELVIN;
+                            break;
+
+                        case "Kelvin":
+                            resultTemp = inputTemp;
+                            break;
+                    }
+                    lblResult.Text = resultTemp.ToString("n2") + " K";
+                    break;
+            }
+
+
+        }
+
+        
+    }
+}
+
